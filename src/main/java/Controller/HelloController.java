014ee3c589321e94cbe7a5309913a33327e1bb0e
mikiastadesse1 <mikias.tadesse@adp.com>
2025-08-12 11:15:10 -0400

    test commit

--- src/main/java/Controller/HelloController.java
+++ src/main/java/Controller/HelloController.java
@@ -1,9 +1,14 @@
 package Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
 
